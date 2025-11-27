cc7b7e0c034580cc1dd25033dbafa0cdff5cc09b
Alvar <alvarom.rod@gmail.com>
2025-11-27 09:56:45 +0100

    Bump version to 1.6 (versionCode 6) for Play Console release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.pep1lo.bakerspercentagecalculator"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.5"
+        versionCode = 6
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
